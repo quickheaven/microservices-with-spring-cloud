da4677de17b156af421304ec047f412715ecedae
ajie.io <ajie.io@yahoo.com>
2024-12-22 17:26:03 -0500

    Spring Cloud Configuration - Centralized Version Configuration.

---
+++ lab-3/client/src/main/java/ca/quickheaven/lab3/config/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package ca.quickheaven.lab3.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+}
